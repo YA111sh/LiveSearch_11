00bab8b02f7f9779df52c1f6e4fa3bef09e41aba
yash prajapati <yash prajapati@DESKTOP-3TVAUS8>
2019-09-15 09:44:57 +0530

    Add project files.

---
+++ LiveSearch/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using LiveSearch.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LiveSearch.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            LiveDataEntities db = new LiveDataEntities();
+            
+            return View(db.UserMasters.ToList());
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ LiveSearch/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LiveSearch.MvcApplication" Language="C#" %>
